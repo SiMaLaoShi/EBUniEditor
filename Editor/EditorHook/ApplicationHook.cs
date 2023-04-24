506be4a59326924039818e4f4b710156c5b83aba
simalaoshi <1742495214@qq.com>
2023-04-24 18:06:16 +0800

    1，增加MonoHook相关

---
+++ Editor/EditorHook/ApplicationHook.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using EBA.Ebunieditor.Editor.Common;
+using MonoHook;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+
+#endif
+
+namespace EBA.Ebunieditor.Editor.EditorHook
+{
+#if UNITY_EDITOR
+    public class ApplicationHook
+    {
+        public static string oldStreamingAssetsPath = Application.streamingAssetsPath;
+        public static string oldPersistentDataPath = Application.persistentDataPath;
+
+        public static string streamingAssetsPath
+        {
+            get
+            {
+                if (!GlobalScriptableObject.Instance.isHookApplication)
+                    return oldStreamingAssetsPath;
+                var p = GlobalScriptableObject.Instance.strRemoteStreamingAssetsPath;
+                return p == "" ? oldStreamingAssetsPath : p;
+            }
+        }
+
+        public static string persistentDataPath
+        {
+            get
+            {
+                if (!GlobalScriptableObject.Instance.isHookApplication)
+                    return oldPersistentDataPath;
+                var p = GlobalScriptableObject.Instance.strRemotePersistentDataPath;
+                return p == "" ? oldPersistentDataPath : p;
+            }
+        }
+
+        [Serializable]
+        class HookInfo
+        {
+            public string OldMethod;
+            public string NewMethod;
+            public string ProxyMethod;
+            public Type SrcType;
+            public Type DestType;
+        }
+        static BindingFlags bindingFlags = BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic |
+                                    BindingFlags.Instance;
+
+        private static List<MethodHook> MethodHooks = new List<MethodHook>();
+#if UNITY_5 || UNITY_2017_1_OR_NEWER
+        //这个属性在场景加载后直接启动我们的方法
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+#endif
+        public static void OnStartGame()
+        {
+            EditorApplication.playModeStateChanged += OnPlayerModeStateChanged;
+            if (!GlobalScriptableObject.Instance.isHookApplication)
+                return;
+
+            List<HookInfo> hookInfos = new List<HookInfo>()
+            {
+                new HookInfo()
+                {
+                    OldMethod = "get_streamingAssetsPath",
+                    NewMethod = "get_streamingAssetsPath",
+                    ProxyMethod = "GetStreamingAssetsPathProxy",
+                    SrcType = typeof(Application),
+                    DestType = typeof(ApplicationHook),
+                },
+                new HookInfo()
+                {
+                    OldMethod = "get_persistentDataPath",
+                    NewMethod = "get_persistentDataPath",
+                    ProxyMethod = "GetPersistentDataPathProxy",
+                    SrcType = typeof(Application),
+                    DestType = typeof(ApplicationHook),
+                },
+            };
+            
+            foreach (var hookInfo in hookInfos)
+            {
+                var target = hookInfo.SrcType.GetMethod(hookInfo.OldMethod, bindingFlags);
+                var dest = hookInfo.DestType.GetMethod(hookInfo.NewMethod, bindingFlags);
+                var proxy = hookInfo.DestType.GetMethod(hookInfo.ProxyMethod, bindingFlags);
+                var hook = new MethodHook(target, dest, proxy);
+                hook.Install();
+                MethodHooks.Add(hook);
+            } 
+            Debug.Log("Application.persistentDataPath:" + Application.persistentDataPath);
+            Debug.Log("Application.streamingAssetsPath:" + Application.streamingAssetsPath);
+        }
+
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        private static string GetStreamingAssetsPathProxy()
+        {
+            return streamingAssetsPath;
+        }
+        
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        private static string GetPersistentDataPathProxy()
+        {
+            return persistentDataPath;
+        }
+
+        static void OnPlayerModeStateChanged(PlayModeStateChange playModeStateChange)
+        {
+            if (playModeStateChange == PlayModeStateChange.ExitingPlayMode)
+            {
+                foreach (var methodHook in MethodHooks)
+                {
+                    methodHook.Uninstall();
+                }
+            }
+        }
+    }
+#endif
+}
---
+++ Editor/EditorHook/Hook/Scripts/TestA.cs
@@ -0,0 +1,156 @@
+﻿using DotNetDetour;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+#if ENABLE_HOOK_TEST_CASE
+using MonoHook.Test;
+#endif
+
+namespace MonoHook
+{
+    public unsafe class TestA   : MonoBehaviour
+    {
+        public Button btn, btnDelFile;
+        public Text txtInfo;
+        public Text txtTestVal;
+
+#region test case
+#if ENABLE_HOOK_TEST_CASE
+
+        private int _msgId;
+
+        private void Awake()
+        {
+            btn.onClick.AddListener(OnBtnTestClick);
+            btnDelFile.onClick.AddListener(OnBtnTestDelFileClick);
+        }
+        private void Start()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("pointer size:{0}\r\n", System.IntPtr.Size);
+            sb.AppendFormat("is IL2CPP:{0}\r\n", LDasm.IsIL2CPP());
+            sb.AppendFormat("operation name:{0}\r\n", SystemInfo.operatingSystem);
+            sb.AppendFormat("processorType:{0}\r\n", SystemInfo.processorType);
+            sb.AppendLine();
+            txtInfo.text = sb.ToString();
+        }
+
+        public void OnBtnTestDelFileClick()
+        {
+#if UNITY_EDITOR
+            string fileName = "test.txt";
+            string dirPath = "Assets/test_dir";
+            string filePath1 = $"Assets/{fileName}";
+            string filePath2 = $"{dirPath}/{fileName}";
+            {
+                File.Create(filePath1).Close();
+                File.Delete(filePath1);
+            }
+            {
+                Directory.CreateDirectory(dirPath);
+                File.Create(filePath2).Close();
+                Directory.Delete(dirPath, true);
+            }
+            {
+                File.Create(filePath1).Close();
+                AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+                AssetDatabase.DeleteAsset(filePath1);
+            }
+#else
+            Debug.Log("disable DelFile Hook at runtime");
+#endif
+        }
+
+        public void OnBtnTestClick()
+        {
+            Debug.Log("Test Begin");
+
+            // 实例方法替换测试
+            {
+                InstanceMethodTest.InstallPatch();
+                InstanceMethodTest.callOriFunc = true;
+                InstanceMethodTest InstanceTest = new InstanceMethodTest();
+                InstanceTest.Test();
+                int ret = InstanceTest.Test();
+
+                int count = 10 * 1000 * 1000;
+                string str = $"InstanceMethodTest {count} times, ret: {ret}\r\n";
+
+                DateTime dt = DateTime.Now;
+                long val = 0;
+                {// patch and call original function
+                    for (int i = 0; i < count; i++)
+                    {
+                        val += InstanceTest.Test();
+                    }
+                    str += $"patch:      {(int)new TimeSpan(DateTime.Now.Ticks - dt.Ticks).TotalMilliseconds} ms, ret:{val}\r\n";
+                }
+
+                {// patch but dont call original function
+                    dt = DateTime.Now;
+                    val = 0;
+                    InstanceMethodTest.callOriFunc = false;
+                    for (int i = 0; i < count; i++)
+                    {
+                        val += InstanceTest.Test();
+                    }
+                    str += $"patch, no ori call:{(int)new TimeSpan(DateTime.Now.Ticks - dt.Ticks).TotalMilliseconds} ms, ret:{val}\r\n";
+                }
+
+                {// no patch
+                    InstanceMethodTest.UninstallPatch();
+                    dt = DateTime.Now;
+                    val = 0;
+                    for (int i = 0; i < count; i++)
+                    {
+                        val += InstanceTest.Test();
+                    }
+                    str += $"no patch: {(int)new TimeSpan(DateTime.Now.Ticks - dt.Ticks).TotalMilliseconds} ms, ret:{val}";
+                }
+
+                txtTestVal.text = str;
+            }
+
+            // 属性替换测试
+            PropertyHookTest propTest = new PropertyHookTest();
+            propTest.Test();
+
+            // 参数类型是私有类型的方法替换测试
+            PrivateTypeArgMethodTest privateTypeArgMethodTest = new PrivateTypeArgMethodTest();
+            privateTypeArgMethodTest.Test();
+
+            // 构造函数替换测试
+            CtorHookTest ctorHookTest = new CtorHookTest();
+            ctorHookTest.Test();
+
+            // 测试GameObject.SetActive
+            GameObject_SetActive_HookTest.Test(btn.gameObject);
+
+            {// 泛型方法 hook 测试，目前仅支持 il2cpp
+                GenericMethod_HookTest.Install();
+
+                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                Instantiate((UnityEngine.Object)go);
+
+                GameObject goNew = Instantiate<UnityEngine.Object>(go) as GameObject;
+                goNew.name = "hook_go_Instantiate";
+                goNew.transform.localPosition = new Vector3(-5f, 0, 0);
+                Destroy(go);
+                Destroy(goNew);
+            }
+            Debug.Log("Test End");
+        }
+#endif
+#endregion
+    }
+}
+
