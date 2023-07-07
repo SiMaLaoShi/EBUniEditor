8cc4ad960c0a1aab57ff9cd1d57432b722923bbd
simalaoshi <1742495214@qq.com>
2023-07-07 18:12:18 +0800

    1，调整目录结构相关

---
+++ Runtime/UGUI/NoDrawingRayCast.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//注：unity3dUGUI空对象实现射线检测  
+//参考 https://blog.csdn.net/weixin_33943347/article/details/89591903
+namespace EBA.Runtime.UGUI
+{
+    public class NoDrawingRayCast : Graphic
+    {
+        protected NoDrawingRayCast()
+        {
+            useLegacyMeshGeneration = false;
+        }
+
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+        }
+    }
+}
