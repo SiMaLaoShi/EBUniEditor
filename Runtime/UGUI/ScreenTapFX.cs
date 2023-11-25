1593b8cfc452baa01dc54a32c3d69ad1cefb0dc8
simalaoshi <1742495214@qq.com>
2023-11-25 15:31:56 +0800

    1，添加一个最顶层的Ping脚本

---
+++ Runtime/UGUI/ScreenTapFX.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+#if UNITY_EDITOR
+#endif
+
+namespace EBUniEditor.Runtime.UGUI
+{
+    public class ScreenTapFX : MonoBehaviour
+    {
+
+        private Vector2 point;
+        public Canvas Canvas;
+        public GameObject EffectEntity;
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //将鼠标点击的屏幕坐标转换为UI坐标，最后一个输出参数为转换的点
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(Canvas.transform as RectTransform,
+                    Input.mousePosition, Canvas.worldCamera, out point);
+                var go = Instantiate(EffectEntity);
+                go.GetComponent<RectTransform>().anchoredPosition = point;
+#if UNITY_EDITOR
+                EditorGUIUtility.PingObject(EventSystem.current.currentSelectedGameObject);          
+#endif
+            }
+        }
+    }
+}
